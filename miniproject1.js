468122a5a4cabd8bd66a5569e77a02df3055fdfe
Karanspinkwood <135384575+Karanspinkwood@users.noreply.github.com>
2023-07-10 20:43:09 -0600

    Update miniproject1.js

--- miniproject1.js
+++ miniproject1.js
@@ -31,12 +31,12 @@ const Iundergrad = {
     }
   }
   
-  // Associate the Undergraduates class with the Iundergrad interface-like implementation
+  // Associating the Undergraduates class with the Iundergrad interface-like implementation
   class JohnWick extends Undergraduates {}
   
-  // Create an Undergraduate instance (John Wick)
+  // Creating an Undergraduate instance (John Wick)
   const johnWick = new JohnWick("John Wick", 58, "Male", "USA", 2003, 4.0);
   
-  // Print John Wick's nationality in the console
+  // Printing John Wick's nationality in the console
   johnWick.getNationality();
   
